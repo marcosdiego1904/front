dc1e4f9c80ddec59765647f350de1174b0439206
marcosdiego1904 <76596163+marcosdiego1904@users.noreply.github.com>
2025-06-04 16:15:55 -0400

    mov

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -115,9 +115,9 @@ const Home = () => {
               </button>
             </div>
 
-            <p style={{ marginTop: "40px" }}>
-              Memorize your first verse in under 5 minutes – challenge yourself today!
-            </p>
+            <p>
+  Memorize your first verse in under 5 minutes – challenge yourself today!
+</p>
           </div>
         </div>
         <div className="right">
